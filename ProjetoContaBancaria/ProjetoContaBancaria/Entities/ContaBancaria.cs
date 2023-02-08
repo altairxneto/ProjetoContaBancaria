8d8096d78a19886347f7491488da8edcce5aeb1b
Altair Garcia de Siqueira Neto <altairxneto@gmail.com>
2023-02-08 10:58:17 -0300

    Implementei a classe ContaBancaria

---
+++ ProjetoContaBancaria/ProjetoContaBancaria/Entities/ContaBancaria.cs
@@ -0,0 +1,10 @@
+﻿
+using ContaBancaria.Entities.Enums;
+
+namespace ContaBancaria.Entities {
+    class ContaBancaria {
+        public int NumeroConta { get; private set; }
+        public int AgenciaConta { get; private set; }
+
+    }
+}
