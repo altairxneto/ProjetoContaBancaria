67713398305e16ad9c8ce75f1ca7b91d20f8631a
Altair Garcia de Siqueira Neto <altairxneto@gmail.com>
2023-02-08 10:59:16 -0300

    Implementei a classe Pessoa

---
+++ ProjetoContaBancaria/ProjetoContaBancaria/Entities/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using ProjetoContaBancaria.Entities.Enums;
+
+namespace ContaBancaria.Entities
+{
+    public class Pessoa {
+        public string Nome { get; set; }
+        public string NomeDoPai { get; set; }
+        public string NomeDaMae { get; set; }
+        public int Idade { get; private set; }
+        public Sexo Sexo { get; set; }
+        public DateTime DataDeNascimento { get; private set; }
+        public EstadoCivil EstadoCivil { get; private set; }
+
+        public Pessoa(string nome, string nomeDoPai, string nomeDaMae, Sexo sexo, DateTime dataDeNascimento, EstadoCivil estadoCivil) {
+            Nome = nome;
+            NomeDoPai = nomeDoPai;
+            NomeDaMae = nomeDaMae;
+            Sexo = sexo;
+            DataDeNascimento = dataDeNascimento;
+            EstadoCivil = estadoCivil;
+            TimeSpan calcularDiasDeVidaDaPessoa = DateTime.Now.Subtract(DataDeNascimento);
+            Idade = (int)calcularDiasDeVidaDaPessoa.TotalDays/365;
+        }
+    }
+}
